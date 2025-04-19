ba19a9fbc4ff11654decc4e4fda0a21b1fd7455f
Incubator <461462406@qq.com>
2025-04-19 23:56:40 +0800

    1

--- HotEthanol/STRINGS/CREATURES.cs
+++ HotEthanol/STRINGS/CREATURES.cs
@@ -8,8 +8,7 @@ public class CREATURES
         // Strings.Add($"STRINGS.CREATURES.SPECIES.GEYSER.{GeyserEthanol.HotEthanol.ToUpper()}.DESC", GeyserEthanol.DESC);
         public class HOT_ETHANOL
         {
-                public static LocString NAME = (LocString) UI.FormatAsLink("Ethanol Vapor Vent", "GeyserGeneric_HOT_ETHANOL");
-                public static LocString DESC = (LocString) ("A highly pressurized vent that periodically erupts with boiling " + UI.FormatAsLink("Ethanol", "ETHANOL") + ".");
-
+                public static LocString NAME = (LocString) UI.FormatAsLink("Hot Ethanol Vapor Vent", "GeyserGeneric_HOT_ETHANOL");
+                public static LocString DESC = (LocString) ("A highly pressurized vent that periodically erupts with scalding " + UI.FormatAsLink("Ethanol Vapor", "ETHANOLGAS") + ".");
         }
 }
